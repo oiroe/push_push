7f2f87904918c8360fbef8a4ec6e7211e39e0f34
oiroe <pathiti.b@gmail.com>
2023-06-08 01:28:04 +0700

    meaw

--- test.c
+++ test.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
+void	sort_int_tab(int *tab, unsigned int size)
+{
+	unsigned int	i = 0;
+	int	temp;
+
+	while (i < (size - 1))
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			temp = tab[i];
+			tab[i] = tab[i+ 1];
+			tab[i + 1] = temp;
+			i = 0;
+		}
+		else
+			i++;
+	}
+}
+
 int main()
 {
-	printf("%d\n", -36>>2);
+	int	i[] = {2, 1, 7, 4, 6, 5, 3};
+	sort_int_tab(i, 7);
+	int	j;
+	j = 0;
+	while (j < 7)
+	{
+		printf("%d\n", i[j]);
+		j++;
+	}
 	return (0);
 }
